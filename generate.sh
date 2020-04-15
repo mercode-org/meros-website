f20441babfa4aeb662623ea8bcd5a80c5601ccb2
Maciej Krüger <mkg20001@gmail.com>
2020-04-15 23:23:23 +0200

    feat: replace scripts with new npm based scripts

--- generate.sh
+++
@@ -1,5 +0,0 @@
-echo Copyleft 2019-2020 Mercode - Protected by GPLv3 certificate.
-npm install --package-lock-only --no-package-lock pug
-pug -w ./ -o ./os.mercode.org
-pug -w ./tr -o ./os.mercode.org/tr
-echo Done!
